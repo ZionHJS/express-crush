ce0ab6eda52621cefeab5966a3b49a0d24c6da50
zionHuang <hjszion@gmail.com>
2019-09-19 22:10:03 -0700

    updated lots

--- index.js
+++ index.js
@@ -1,13 +1,28 @@
 const express = require('express');
 const path = require('path');
+const exphbs = require('express-handlebars');
 const logger = require('./middleware/logger');
+const members = require('./Members');
 
 const app = express();
 
 //init middleware logger
 app.use(logger);
 
-//Set static folder then we can put lots of static pages under it!
+//Handlebars Middleware
+app.engine('handlebars', exphbs({defaultLayout:"main"}));
+app.set('view engine', 'handlebars');
+
+//Body Parser Middldware to help handle urlencoded data
+app.use(express.json());
+app.use(express.urlencoded({extended:false}));
+
+//Homepage Route
+app.get('/', (req, res) => res.render('index', {
+    title:'Member App';
+}));
+
+//Set static folder then we can put lots of static pages in it!
 app.use(express.static(path.join(__dirname, 'public')));
 
 //members api routes
